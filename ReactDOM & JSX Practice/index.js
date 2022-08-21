cf5a42a11fe056be2cc40b45297cbd73b8d5b38b
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-08-21 08:35:10 +0200

    Add files via upload

---
+++ ReactDOM & JSX Practice/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <ul>
+    <li>Carlos</li>
+    <li>Jose</li>
+    <li>Maria</li>
+  </ul>,
+  document.getElementById("root")
+)
