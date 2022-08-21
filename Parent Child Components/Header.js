5d0ac367bfca16495e40e0ea71571a28663bec97
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-08-21 08:43:59 +0200

    Add files via upload

---
+++ Parent Child Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (
+        <nav>
+            <h1>Hello a third time!</h1>
+        </nav>
+    )
+}
+
+export default Header
---
+++ Parent Child Components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <ul>
+            <li>Thing 1</li>
+            <li>Thing 2</li>
+            <li>Thing 3</li>
+        </ul>
+    )
+}
+
+export default MainContent
