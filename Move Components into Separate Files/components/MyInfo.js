dcc51d226a0658f63f132558dec71d33f72db7d1
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-08-21 08:39:01 +0200

    Add files via upload

---
+++ Move Components into Separate Files/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Carlos Infante</h1>
+      <p>This is a paragraph about me...</p>
+      <ul>
+        <li>Cubs</li>
+        <li>Nehterlands</li>
+        <li>Rest of the World</li>
+      </ul>
+    </div>
+  )
+}
+//to make available this in the index.js, we export it
+export default MyInfo
---
+++ Move Components into Separate Files/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//to grab MyInfo function. In this Framework is also used camel notation but srtarting with capital letters
+import MyInfo from "./components/MyInfo"
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
